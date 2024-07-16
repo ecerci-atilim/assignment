e411b0a19db6326ea2c2dd3563d603567fe0225c
ecerci-atilim <emre.cerci@atilim.edu.tr>
2024-07-16 16:37:00 +0300

    final code

--- main.c
+++ main.c
@@ -3,50 +3,99 @@
 #include <dirent.h>
 #include <string.h>
 #include <stdlib.h>
+#include <stdbool.h>
+#include <unistd.h>
 
-#define FNAME_LEN 10
+#define FNAME_LEN 6
 #define DUMMY_SIZE 1<<8
+#define BIG_SIZE 1<<10
+#define SMALL_SIZE 1<<4
+#define PATH_SIZE 1<<8
 
 int getID(char strin[]);
 int getBase(char strin[]);
 int getLength(char strin[]);
 void getStr(char strin[], int buflen, char **bufout);
+void processStr(char l1[], char l2[], char l3[], int len, char **processed);
 
 int main (void)
 {
-     //system("cls");
+     system("cls");
      DIR *dirptr;
+     FILE *fiddb, *fidtemp;
      char *fname = (char *)malloc(FNAME_LEN);
-     char dummy[DUMMY_SIZE];
-     FILE *fid;
+     char dummy[DUMMY_SIZE], l1[BIG_SIZE], l2[BIG_SIZE], l3[BIG_SIZE], t1[SMALL_SIZE], t2[SMALL_SIZE];
+     char path[PATH_SIZE];
      int fcnt = 0;
-     int fileIDtemp;
+     int fileIDtemp, basetemp, lentemp;
+     char *strtemp, *checkStr;
+     bool mainSuccess = true, tempSuccess;
 
      // Database
-     fid = fopen("nums.db", "r");
-     fgets(dummy, DUMMY_SIZE, fid);
-     fclose(fid);
-     fileIDtemp = getLength(dummy);
-     printf("%d: ", fileIDtemp);
-     char *t = malloc(2);
-     getStr(dummy, fileIDtemp, &t);
-     printf("%s\n", t);
-
+     fiddb = fopen("nums.db", "r");
+     char *t = (char *)malloc(DUMMY_SIZE);
 
      struct dirent *entry;     
      dirptr = opendir ("./num/");
      if (dirptr != NULL){
           while ((entry = readdir (dirptr)) != NULL){
+               tempSuccess = true;
                fname = entry->d_name;
                if(fname[0] != 46){
-                    //printf("Processing %s, file #%d\n", fname, fcnt);
+                    printf("Processing %s, file #%d\n", fname, fcnt);
                     // Processing
+                    (void)fgets(dummy, sizeof(dummy), fiddb);
+                    fileIDtemp = getID(dummy);
+                    basetemp = getBase(dummy);
+                    lentemp = getLength(dummy);
+                    getStr(dummy, lentemp, &t);
+                    printf("dB Info:\n\tdB ID:    %d\n\tdB base:  %d\n\tdB len:   %d\n\tdB value: ", fileIDtemp, basetemp, lentemp);
+                    for(int i = 0; i < lentemp; i++)
+                         printf("%c", *(t+i));
+                    printf("\nProcess Info:\n");
+                    if ((size_t)snprintf(path, sizeof(path), "./num/%s", entry->d_name) >= sizeof(path)) {
+                         printf("Filename too long!");
+                         continue;
+                    } 
+                    printf("\tFile path: %s\n", path);
+
+                    fidtemp = fopen(path, "r");
+                    if(fidtemp==NULL) printf("fid null");
+                    (void)fgets(t1, sizeof(t1), fidtemp);
+                    (void)fgets(t2, sizeof(t2), fidtemp);
+                    (void)fgets(l1, sizeof(l1), fidtemp);
+                    (void)fgets(l2, sizeof(l2), fidtemp);
+                    (void)fgets(l3, sizeof(l3), fidtemp);
+                    processStr(l1, l2, l3, lentemp, &strtemp);
+                    printf("\tValue   : ");
+                    for(int i = 0; i < lentemp; i++){
+                         printf("%c",*(strtemp + i));
+                         if(*(strtemp + i) != *(t+i))
+                              tempSuccess = false;
+                    }
+                    if(tempSuccess == false){
+                         printf("\n\tStatus  : Failed");
+                         mainSuccess = false;
+                    }
+                    else printf("\n\tStatus  : Success");
+
+
+                    (void)fclose(fidtemp);
+                    printf("\n\n----------\n\n");
                     fcnt++;
+                    usleep((unsigned int)(1<<20));
                }
           }
                
-     (void) closedir(dirptr);
+     (void)closedir(dirptr);
+     (void)fclose(fiddb);
+
+     if(mainSuccess)
+          printf("Files successfully processed. \nFile contents match exactly with database.");
+     else
+          printf("Execution ended with errors.");
      return 0;
+
      }
      else{
           return -1;
@@ -70,7 +119,7 @@ int getID(char strin[]){
 
 int getBase(char strin[]){
      int htab1 = 0, i, c, sc = 0;
-     char base[2];
+     char base[3];
      for(i = 0; i < strlen(strin); i++){
           if(strin[i] == 9){
                htab1 = i;
@@ -121,4 +170,86 @@ void getStr(char strin[], int buflen, char **bufout){
           sc++;
      }
      *bufout = buftemp;
+}
+
+void processStr(char l1[], char l2[], char l3[], int len, char **processed){
+     char *proctemp = malloc(len*sizeof(char));
+     int i1, i2, i3;
+
+     for(int i = 0; i < len; i++){
+          i1 = i*4;
+          i2 = i1 + 1;
+          i3 = i2 + 1;
+          
+          if(
+               l1[i1]==32 && l1[i2]!=32 && l1[i3]==32 &&
+               l2[i1]!=32 && l2[i2]==32 && l2[i3]!=32 &&
+               l3[i1]!=32 && l3[i2]!=32 && l3[i3]!=32)      *(proctemp + i) = '0';
+          else if(
+               l1[i1]==32 && l1[i2]==32 && l1[i3]==32 &&
+               l2[i1]==32 && l2[i2]==32 && l2[i3]!=32 &&
+               l3[i1]==32 && l3[i2]==32 && l3[i3]!=32)      *(proctemp + i) = '1';
+          else if(
+               l1[i1]==32 && l1[i2]!=32 && l1[i3]==32 &&
+               l2[i1]==32 && l2[i2]!=32 && l2[i3]!=32 &&
+               l3[i1]!=32 && l3[i2]!=32 && l3[i3]==32)      *(proctemp + i) = '2';
+          else if(
+               l1[i1]==32 && l1[i2]!=32 && l1[i3]==32 &&
+               l2[i1]==32 && l2[i2]!=32 && l2[i3]!=32 &&
+               l3[i1]==32 && l3[i2]!=32 && l3[i3]!=32)      *(proctemp + i) = '3';
+          else if(
+               l1[i1]==32 && l1[i2]==32 && l1[i3]==32 &&
+               l2[i1]!=32 && l2[i2]!=32 && l2[i3]!=32 &&
+               l3[i1]==32 && l3[i2]==32 && l3[i3]!=32)      *(proctemp + i) = '4';
+          else if(
+               l1[i1]==32 && l1[i2]!=32 && l1[i3]==32 &&
+               l2[i1]!=32 && l2[i2]!=32 && l2[i3]==32 &&
+               l3[i1]==32 && l3[i2]!=32 && l3[i3]!=32)      *(proctemp + i) = '5';
+          else if(
+               l1[i1]==32 && l1[i2]!=32 && l1[i3]==32 &&
+               l2[i1]!=32 && l2[i2]!=32 && l2[i3]==32 &&
+               l3[i1]!=32 && l3[i2]!=32 && l3[i3]!=32)      *(proctemp + i) = '6';
+          else if(
+               l1[i1]==32 && l1[i2]!=32 && l1[i3]==32 &&
+               l2[i1]==32 && l2[i2]==32 && l2[i3]!=32 &&
+               l3[i1]==32 && l3[i2]==32 && l3[i3]!=32)      *(proctemp + i) = '7';
+          else if(
+               l1[i1]==32 && l1[i2]!=32 && l1[i3]==32 &&
+               l2[i1]!=32 && l2[i2]!=32 && l2[i3]!=32 &&
+               l3[i1]!=32 && l3[i2]!=32 && l3[i3]!=32)      *(proctemp + i) = '8';
+          else if(
+               l1[i1]==32 && l1[i2]!=32 && l1[i3]==32 &&
+               l2[i1]!=32 && l2[i2]!=32 && l2[i3]!=32 &&
+               l3[i1]==32 && l3[i2]!=32 && l3[i3]!=32)      *(proctemp + i) = '9';
+          else if(
+               l1[i1]==32 && l1[i2]!=32 && l1[i3]==32 &&
+               l2[i1]!=32 && l2[i2]!=32 && l2[i3]!=32 &&
+               l3[i1]!=32 && l3[i2]==32 && l3[i3]!=32)      *(proctemp + i) = 'A';
+          else if(
+               l1[i1]==32 && l1[i2]==32 && l1[i3]==32 &&
+               l2[i1]!=32 && l2[i2]!=32 && l2[i3]==32 &&
+               l3[i1]!=32 && l3[i2]!=32 && l3[i3]!=32)      *(proctemp + i) = 'B';
+          else if(
+               l1[i1]==32 && l1[i2]!=32 && l1[i3]==32 &&
+               l2[i1]!=32 && l2[i2]==32 && l2[i3]==32 &&
+               l3[i1]!=32 && l3[i2]!=32 && l3[i3]==32)      *(proctemp + i) = 'C';
+          else if(
+               l1[i1]==32 && l1[i2]==32 && l1[i3]==32 &&
+               l2[i1]==32 && l2[i2]!=32 && l2[i3]!=32 &&
+               l3[i1]!=32 && l3[i2]!=32 && l3[i3]!=32)      *(proctemp + i) = 'D';
+          else if(
+               l1[i1]==32 && l1[i2]!=32 && l1[i3]==32 &&
+               l2[i1]!=32 && l2[i2]!=32 && l2[i3]==32 &&
+               l3[i1]!=32 && l3[i2]!=32 && l3[i3]==32)      *(proctemp + i) = 'E';
+          else if(
+               l1[i1]==32 && l1[i2]!=32 && l1[i3]==32 &&
+               l2[i1]!=32 && l2[i2]!=32 && l2[i3]==32 &&
+               l3[i1]!=32 && l3[i2]==32 && l3[i3]==32)      *(proctemp + i) = 'F';
+               else *(proctemp + i) = '?';
+
+     }
+
+     *processed = proctemp;
+     
+
 }
